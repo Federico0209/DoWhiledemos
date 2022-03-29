a29916dc610c69ff0c03db85e8ef61592ec2d568
Federico <Federico@FEDEDELL>
2022-03-29 17:43:46 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoWhiledemos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //do
+            //enunciados
+            //while (epresión boolena);
+
+            do
+            {
+                Console.WriteLine("Hola");
+            }
+            while (8 > 10);
+
+            int i = 0;
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+
+            }
+            while (i <= 10);
+
+            int j = 0;
+            int suma = 0;
+
+            do
+            {
+                suma += j;
+                j++;
+
+            }
+            while (j <= 10);
+
+            Console.WriteLine(suma);
+            Console.ReadLine();
+        }
+    
+    
+    }
+
+}
